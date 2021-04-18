e17f08101f4caa2635b98ccda6e8787ded8c44d3
Suleymanovdavid <66300375+Suleymanovdavid@users.noreply.github.com>
2021-04-18 09:42:24 +0500

    Update and rename week 1.dart to max subarray.dart

---
+++ algorithms/max subarray.dart
@@ -0,0 +1,34 @@
+//Finding maximum sub-array algorithm
+
+void max_subarray(array,low, high){
+int mid_point=(array.length)~/2;
+double left=double.negativeInfinity;
+double sum=0;
+for(int i =mid_point; i>=low; i--){
+  sum=sum+array[i];
+  if(sum>left){
+    left=sum;
+    
+  }
+}
+double right=double.negativeInfinity;
+sum=0;
+for(int j=mid_point+1; j<high;j++){
+sum=sum+array[j];
+if(sum>right){
+    right=sum;
+    
+}
+}
+
+print(left+right);
+}
+
+
+void main(){
+
+List <int>array=[-2, -5, 6, -2, -3, 1, 5, -6];
+int low=0;
+int high=array.length;
+max_subarray(array, low, high);
+}
